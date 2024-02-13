db3f5558346d3bb7af542baa628601eb8408c975
kinimody <94915772+kinimody@users.noreply.github.com>
2024-02-13 01:36:28 +0100

    login on mobile improved

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -7,20 +7,16 @@ import { FaGithub } from "react-icons/fa";
 import Image from "next/image";
 
 const Auth = () => {
-
   const [email, setEmail] = useState("");
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
   const [variant, setVariant] = useState("login");
-  
-  
+
   const toggleVariant = useCallback(() => {
     setVariant((currentVariant) =>
       currentVariant === "login" ? "register" : "login"
     );
   }, []);
-
-
 
   const login = useCallback(async () => {
     try {
@@ -40,7 +36,6 @@ const Auth = () => {
         email,
         name,
         password,
-        
       });
       login();
     } catch (error) {
@@ -48,20 +43,22 @@ const Auth = () => {
     }
   }, [email, name, password, login]);
 
-
-
-
   return (
     <div className="relative h-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-cover">
       <div className="bg-black w-full h-full lg:bg-opacity-50">
         <nav className="px-12 py-5">
-          <Image src="/images/logo.png" alt="logo" height={"48"} width={177.56}/>
+          <img
+            src="/images/logo.png"
+            alt="logo"
+            className="w-2/6 h-auto sm:w-[20%] "
+          />
+
           <div className="flex justify-center ">
-            <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-3/5 lg:max-w-md rounded-md w-full md:flex md:flex-col md:items-center">
-              <h2 className="text-white text-4xl mb-8 font-semibold">
+            <div className="bg-black bg-opacity-70 sm:px-16 sm:py-16 self-center mt-2  lg:w-3/5 lg:max-w-md rounded-md w-full md:flex md:flex-col md:items-center">
+              <h2 className="text-white text-2xl sm:text-4xl mb-8 mt-10 sm:mt-0 font-semibold">
                 {variant === "login" ? "Sign in" : "Register"}
               </h2>
-              <div className="flex flex-col gap-4 md:w-3/5 lg:w-full">
+              <div className="flex flex-col gap-4 w-full md:w-3/5 lg:w-full">
                 {variant === "register" && (
                   <Input
                     label="Username"
@@ -87,15 +84,21 @@ const Auth = () => {
               </div>
               <button
                 onClick={variant === "login" ? login : register}
-                className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition md:w-3/5 lg:w-full"
+                className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition  md:w-3/5 lg:w-full"
               >
                 {variant === "login" ? "Login" : "Sign up"}
               </button>
               <div className="lg:w-full md:w-3/5 flex flex-row items-center gap-4 mt-8 justify-center">
-                <div onClick={()=> signIn("google",{callbackUrl:"/profiles"})} className="w-8 h-8 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
+                <div
+                  onClick={() => signIn("google", { callbackUrl: "/profiles" })}
+                  className="w-8 h-8 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition"
+                >
                   <FcGoogle size={30} />
                 </div>
-                <div onClick={()=> signIn("github",{callbackUrl:"/profiles"})} className="w-8 h-8 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
+                <div
+                  onClick={() => signIn("github", { callbackUrl: "/profiles" })}
+                  className="w-8 h-8 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition"
+                >
                   <FaGithub size={30} />
                 </div>
               </div>
@@ -116,6 +119,6 @@ const Auth = () => {
       </div>
     </div>
   );
-}
+};
 
 export default Auth;
